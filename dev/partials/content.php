8883762918c9f694f78952bc2ca6c33e1fb34504
DESKTOP-5QRFS7B\Thorben <thorbenl@smartweb.dk>
2017-07-21 11:13:47 +0200

    Finally done :D

--- dev/partials/content.php
+++ dev/partials/content.php
@@ -1,1 +1,165 @@
-<figure> blababsl </figure>
+<!-- HEADER -->
+<header class="header">
+	<div class="row">
+		<div class="col">
+		wegbvtteghstrhtrhsr
+		</div>
+		<div class="col">
+
+			<div class="row">
+				<div class="col">
+				KAPPA PRIDE HTRBR
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col">
+				KAPPA PRIDE NBBT
+				</div>
+			</div>
+
+		</div>
+	</div>
+</header>
+<!-- /HEADER -->
+
+
+<!-- Jumbotron -->
+<section class="cta-marketing">
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+			  <h1 class="display-3">WELCOME</h1>
+			  <p class="lead">Lorem ipsum dolor sit amet, consecteur.</p>
+			  <p class="lead">
+			    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+			  </p>
+		</div>
+	</div>
+</section>
+<!-- /Jumbotron -->
+
+
+
+<!-- ARTICLES -->
+<section class="articles">
+	<div class="row">
+		<div class="col">
+		wegbvtteghstrhtrhsr
+		</div>
+		<div class="col">
+		wegbvtteghstrhtrhsr
+		</div>
+		<div class="col">
+		wegbvtteghstrhtrhsr
+		</div>
+	</div>
+</section>
+<!-- /ARTICLES -->
+
+
+
+
+
+<!-- CTA -->
+<section class="cta">
+	<div class="row">
+		<div class="col">
+		wegbvtteghstrhtrhsr
+		</div>
+		<div class="col">
+			<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+		</div>
+
+
+	</div>
+</section>
+<!-- /CTA -->
+
+
+
+
+
+<!-- WHY CHOOSE US -->
+<section class="choose_us">
+	<div class="row">
+
+		<div class="col">
+
+			<div class="row">
+				<div class="col">
+					testsetstesteste
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					testsetstesteste
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					testsetstesteste
+				</div>
+			</div>
+
+		</div>
+
+		<div class="col">
+
+			<div class="row">
+				<div class="col">
+					testsetstesteste
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					testsetstesteste
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					testsetstesteste
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</section>
+<!-- /WHY CHOOSE US -->
+
+
+
+<!-- SERVICES -->
+<section class="services">
+	<div class="row">
+
+
+
+	</div>
+</section>
+<!-- /SERVICES -->
+
+
+
+
+<!-- REVIEWS -->
+<section class="reviews">
+	<div class="row">
+
+		<div class="col">
+			testsetstesteste
+		</div>
+		<div class="col">
+			testsetstesteste
+		</div>
+		<div class="w-100"></div>
+		<div class="col">
+			testsetstesteste
+		</div>
+		<div class="col">
+			testsetstesteste
+		</div>
+
+	</div>
+</section>
+<!-- /REVIEWS -->
--- dev/partials/head.php
+++ dev/partials/head.php
@@ -7,10 +7,13 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <!-- STYLESHEETS -->
     <link rel="stylesheet" type="text/css" href="assets/css/mainstyle.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
     <!-- /STYLESHEETS -->
 
     <!-- SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <!-- /SCRIPTS -->
 </head>
 <body>
