4126133c879b85d40830cf04343c9a9bbbc94c2e
turbzz <thorben.untamed93@gmail.com>
2017-07-22 16:43:35 +0200

    Set of SCSS changes

--- dev/partials/content.php
+++ dev/partials/content.php
@@ -1,23 +1,104 @@
 <!-- HEADER -->
 <header class="header">
-	<div class="row">
-		<div class="col">
-		wegbvtteghstrhtrhsr
-		</div>
-		<div class="col">
-
-			<div class="row">
-				<div class="col">
-				KAPPA PRIDE HTRBR
+	<div class="container">
+		<div class="row">
+			<div class=" col-md-3 col-lg-2">
+				<div class="logo">
+					<img src="assets/imgs/logo2.png" class="img-fluid"></>
 				</div>
 			</div>
+			<div class="col-lg-8 offset-lg-2">
 
-			<div class="row">
-				<div class="col">
-				KAPPA PRIDE NBBT
+				<div class="row">
+					<div class="col-3">
+					    <div class="input-group">
+					      <span class="input-group-btn" id="sizing-addon2">
+					        <button class="btn btn-secondary" type="button"><i class="fa fa-phone" aria-hidden="true"></i></button>
+					      </span>
+					      <input class="form-control" placeholder="+421 907 617 073">
+					    </div>
+					</div>
+					<div class="col-3">
+					    <div class="input-group">
+					      <span class="input-group-btn" id="sizing-addon2">
+					        <button class="btn btn-secondary" type="button"><i class="fa fa-phone" aria-hidden="true"></i></button>
+					      </span>
+					      <input class="form-control" placeholder="+421 907 617 073">
+					    </div>
+					</div>
+					<div class="col-4">
+					    <div class="input-group">
+					      <span class="input-group-btn">
+					        <button class="btn btn-secondary" type="button"><i class="fa fa-envelope-o" aria-hidden="true"></i></button>
+					      </span>
+					      <input class="form-control" placeholder="juraj.rovnak@gmail.com">
+					    </div>
+					</div>
+					<div class="col-1">
+					     <i class="fa fa-facebook-official" aria-hidden="true"></i>
+					</div>
+					<div class="col-1">
+					     <i class="fa fa-twitter" aria-hidden="true"></i>
+					</div>
 				</div>
-			</div>
 
+				<div class="row">
+					<div class="col">
+						<nav class="navbar navbar-toggleable-md navbar-light">
+						  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						    <span class="navbar-toggler-icon"></span>
+						  </button>
+						
+
+						  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+						  	<div class="row">
+						  		<div class="col-lg-9">
+								    <ul class="navbar-nav mr-auto">
+								      <li class="nav-item active">
+								        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+								      </li>
+								      <li class="nav-item">
+								        <a class="nav-link" href="#">News</a>
+								      </li>
+								      <li class="nav-item dropdown">
+								        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products</a>
+								        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								          <a class="dropdown-item" href="#">Durisol</a>
+								          <a class="dropdown-item" href="#">ECOSHINE</a>
+								        </div>
+								      </li>
+								      <li class="nav-item dropdown">
+								        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pricing</a>
+								        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								          <a class="dropdown-item" href="#">Durisol</a>
+								          <a class="dropdown-item" href="#">ECOSHINE</a>
+								        </div>
+								      </li>
+								      <li class="nav-item">
+								        <a class="nav-link" href="#">Gallery</a>
+								      </li>
+								      <li class="nav-item">
+								        <a class="nav-link" href="#">About us</a>
+								      </li>
+								    </ul>
+								</div>
+								<div class="col-lg-3">
+								    <form class="form-inline my-2 my-lg-0">
+									    <div class="input-group">
+									      <input type="text" class="form-control" placeholder="Search for...">
+									      <span class="input-group-btn" id="sizing-addon2">
+									        <button class="btn btn-secondary" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+									      </span>
+									    </div>
+									</form>
+								</div>
+							</div>
+						  </div>
+						</nav>
+					</div>
+				</div>
+
+			</div>
 		</div>
 	</div>
 </header>
@@ -31,7 +112,7 @@
 			  <h1 class="display-3">WELCOME</h1>
 			  <p class="lead">Lorem ipsum dolor sit amet, consecteur.</p>
 			  <p class="lead">
-			    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+			    <a class="btn btn-primary btn-lg" href="#" role="button">Products</a>
 			  </p>
 		</div>
 	</div>
@@ -39,127 +120,261 @@
 <!-- /Jumbotron -->
 
 
+<div class="wrapper move-up">
 
-<!-- ARTICLES -->
-<section class="articles">
-	<div class="row">
-		<div class="col">
-		wegbvtteghstrhtrhsr
+
+	<!-- ARTICLES -->
+	<section class="articles">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<article>
+						<img src="assets/imgs/we-plan-for-you.jpg" class="img-fluid"></>
+						<h2>Heading</h2>
+						<p>L3fr4grtgtrhrh rzhertzherhrwreg34t 34t34t34t34t34t </p>
+						<p><a href="#">Read more</a></p>
+					</article>
+				</div>
+				<div class="col">
+					<article>
+						<img src="assets/imgs/const-worker.jpg" class="img-fluid"></>
+						<h2>Heading</h2>
+						<p>L3fr4grtgtrhrh rzhertzherhrwreg34t 34t34t34t34t34t </p>
+						<p><a href="#">Read more</a></p>
+					</article>
+				</div>
+				<div class="col">
+					<article>
+						<img src="assets/imgs/durisol-house.jpg" class="img-fluid"></>
+						<h2>Heading</h2>
+						<p>L3fr4grtgtrhrh rzhertzherhrwreg34t 34t34t34t34t34t </p>
+						<p><a href="#">Read more</a></p>
+					</article>
+				</div>
+			</div>
 		</div>
-		<div class="col">
-		wegbvtteghstrhtrhsr
+	</section>
+	<!-- /ARTICLES -->
+
+
+
+
+
+	<!-- CTA -->
+	<section class="cta container-fluid bg-faded">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<p>Looking for a quality and affordable construction?</p>
+				</div>
+				<div class="col">
+					<a class="btn btn-primary btn-lg float-right" href="#" role="button">Learn more</a>
+				</div>
+			</div>
 		</div>
-		<div class="col">
-		wegbvtteghstrhtrhsr
+	</section>
+	<!-- /CTA -->
+
+
+
+
+
+	<!-- WHY CHOOSE US -->
+	<section class="choose_us">
+		<div class="container">
+			<div class="row">
+
+				<div class="col">
+					<h1 class="border-seperation">Why choose Us</h1>
+					<div class="row">
+						<div class="col">
+							<h2>Heading</h2>
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							<h2>Heading</h2>
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							<h2>Heading</h2>
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="col">
+					<h1 class="border-seperation">News</h1>
+					<div class="row articles">
+						<div class="col-4">
+							<img src="assets/imgs/350x350.png" class="img-fluid"></>
+						</div>
+						<div class="col">
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<img src="assets/imgs/350x350.png" class="img-fluid"></>
+						</div>
+						<div class="col">
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
 		</div>
-	</div>
-</section>
-<!-- /ARTICLES -->
+	</section>
+	<!-- /WHY CHOOSE US -->
 
 
 
+	<!-- SERVICES -->
+	<section class="services bg-faded">
+		<div class="container">
+			<h1 class="border-seperation">Services That We Offer</h1>
+			<div class="row">
+				<div class="col">		
+					<ul class="nav flex-column">
+					  <li class="nav-item">
+					    <a class="nav-link active" data-toggle="tab" href="#Tab1" role="tab">Tab1</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" data-toggle="tab" href="#Tab2" role="tab">Tab2</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" data-toggle="tab" href="#Tab3" role="tab">Tab3</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" data-toggle="tab" href="#Tab4" role="tab">Tab4</a>
+					  </li>
+					</ul>
+				</div>
+				<div class="col">
+					<div class="tab-content">
+					  <div class="tab-pane active" id="Tab1" role="tabpanel">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren</div>
+					  <div class="tab-pane" id="Tab2" role="tabpanel">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</div>
+					  <div class="tab-pane" id="Tab3" role="tabpanel">Lorem ipsum dolor sit amet.</div>
+					  <div class="tab-pane" id="Tab4" role="tabpanel">.uikuikuikiu..</div>
+					</div>
+				</div>
 
+				<div class="col-3">
+					<img src="assets/imgs/construction-worker.png" class="img-fluid"></>
+				</div>
 
-<!-- CTA -->
-<section class="cta">
-	<div class="row">
-		<div class="col">
-		wegbvtteghstrhtrhsr
+			</div>
 		</div>
-		<div class="col">
-			<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+
+
+	</section>
+	<!-- /SERVICES -->
+
+
+
+
+	<!-- REVIEWS -->
+	<section class="reviews">
+		<div class="container">
+			<div class="row">
+
+
+				<div class="col-6">
+					<div class="row">
+						<div class="col">
+							BLEH BLEH PCITURE AND STARS
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							BLEH BLEH TEXT A LUT
+						</div>
+					</div>
+				</div>
+
+				<div class="col-6">
+					<div class="row">
+						<div class="col">
+							BLEH BLEH PCITURE AND STARS
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							BLEH BLEH TEXT A LUT
+						</div>
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="row">
+						<div class="col">
+							BLEH BLEH PCITURE AND STARS
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							BLEH BLEH TEXT A LUT
+						</div>
+					</div>
+				</div>
+
+				<div class="col-6">
+					<div class="row">
+						<div class="col">
+							BLEH BLEH PCITURE AND STARS
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							BLEH BLEH TEXT A LUT
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
+	</section>
+	<!-- /REVIEWS -->
 
+	<!-- footer -->
+	<footer class="footer bg-faded">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<h2>Where to find us</h2>
+					<iframe></iframe>
+				</div>
+				<div class="col">
+					<h2>Get in touch</h2>
+					<p><i class="fa fa-map-marker" aria-hidden="true"></i>brtbrtbrtbtrbrbrtbt</p>
+					<p><i class="fa fa-envelope-o" aria-hidden="true"></i>brtbrtbrtbtrbrbrtbt</p>
+					<p><i class="fa fa-phone" aria-hidden="true"></i>brtbrtbrtbtrbrbrtbt</p>
 
-	</div>
-</section>
-<!-- /CTA -->
-
-
-
-
-
-<!-- WHY CHOOSE US -->
-<section class="choose_us">
-	<div class="row">
-
-		<div class="col">
+					<div class="col-1">
+						<div class="input-group-btn">
+					     <i class="fa fa-facebook-official" aria-hidden="true"></i>
+					     <i class="fa fa-twitter" aria-hidden="true"></i>
+					    </div>
+					</div>
+				</div>
+				<div class="col">
+					<h2>About us</h2>
+				</div>
+			</div>
 
 			<div class="row">
 				<div class="col">
-					testsetstesteste
+					<p>Created by</p>
 				</div>
-			</div>
-			<div class="row">
 				<div class="col">
-					testsetstesteste
-				</div>
-			</div>
-			<div class="row">
-				<div class="col">
-					testsetstesteste
+					<p>Copyright things</p>
 				</div>
 			</div>
 
 		</div>
-
-		<div class="col">
-
-			<div class="row">
-				<div class="col">
-					testsetstesteste
-				</div>
-			</div>
-			<div class="row">
-				<div class="col">
-					testsetstesteste
-				</div>
-			</div>
-			<div class="row">
-				<div class="col">
-					testsetstesteste
-				</div>
-			</div>
-
-		</div>
-
-	</div>
-</section>
-<!-- /WHY CHOOSE US -->
-
-
-
-<!-- SERVICES -->
-<section class="services">
-	<div class="row">
-
-
-
-	</div>
-</section>
-<!-- /SERVICES -->
-
-
-
-
-<!-- REVIEWS -->
-<section class="reviews">
-	<div class="row">
-
-		<div class="col">
-			testsetstesteste
-		</div>
-		<div class="col">
-			testsetstesteste
-		</div>
-		<div class="w-100"></div>
-		<div class="col">
-			testsetstesteste
-		</div>
-		<div class="col">
-			testsetstesteste
-		</div>
-
-	</div>
-</section>
-<!-- /REVIEWS -->
+	</footer>
+	<!-- /CTA -->
+</div>
--- dev/partials/head.php
+++ dev/partials/head.php
@@ -1,17 +1,25 @@
-
+<!doctype html>
+<html lang="sk">
  <head>
     <title>Rovnak</title>
     <meta charset="utf-8">
+    <meta name="keywords" content="rovnak, durisol, ecoshine, responsive, web, construction, kosice">
+    <meta name="description" content="Mr Rovnak builds very environment friendly houses with good material.">
+    <meta name="author" content="Juraj Rovnak">
+    <meta name="dcterms.rightsHolder" content="Juraj Rovnak">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="#">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <!-- STYLESHEETS -->
     <link rel="stylesheet" type="text/css" href="assets/css/mainstyle.css" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">    
+    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
     <!-- /STYLESHEETS -->
-
+   
     <!-- SCRIPTS -->
+    <script src="https://use.fontawesome.com/a865ed03bc.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <!-- /SCRIPTS -->
